e145ff994a07bc6bd311cfdc869bd6c3354d2b68
zhanglin20 <zhanglin20@staff.weibo.com>
2022-08-05 01:18:10 +0800

    service base
    service base2
    
    service base3

---
+++ service/base.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"context"
+	"fmt"
+)
+
+func GetMemberID(ctx context.Context)  {
+	fmt.Println("GetMemberID")
+}
+
+func GetMember(ctx context.Context)  {
+	fmt.Println("GetMember")
+}
+
+func GetInviteList(ctx context.Context, memberID int64)  {
+	fmt.Println("GetInviteList")
+}
+
+func Join(ctx context.Context, memberID int64)  {
+	fmt.Println("Join")
+}
+
+func Submit(ctx context.Context, memberID int64)  {
+	fmt.Println("Submit")
+}
