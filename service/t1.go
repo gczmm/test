25afe0bfd234270c84215dfb34385c87d9fe4218
zhanglin20 <zhanglin20@staff.weibo.com>
2022-08-08 00:23:49 +0800

    t1&t2

---
+++ service/t1.go
@@ -0,0 +1,12 @@
+package service
+
+import "fmt"
+
+func t1_1()  {
+	fmt.Println("t1_1_1")
+}
+
+
+func t1_2()  {
+	fmt.Println("t1_2")
+}
---
+++ service/t2.go
@@ -0,0 +1,12 @@
+package service
+
+import "fmt"
+
+func t2_1()  {
+	fmt.Println("t2_1")
+}
+
+
+func t2_2()  {
+	fmt.Println("t2_2")
+}
